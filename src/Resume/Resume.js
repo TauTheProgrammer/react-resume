370c4221bc82f886ae3661f00d111a87cc099ec1
Ryan Scott <rscott.72.h@gmail.com>
2022-11-08 15:24:55 -0500

    Reorganizes file structure. Installs jest

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,6 +1,6 @@
 import './Resume.css';
-import Header from './sections/header/Header';
-import Body from './sections/body/Body';
+import Header from './header/Header';
+import Body from './body/Body';
 
 export default function Resume() {
   return (
